3f2d4ff85c9d7e5de1211f8b19c318786686ccb1
Quincey Koziol <koziol@koziol.gov>
2019-12-23 10:34:32 -0600

    Retrieve native info as well as data model info, to put cache into proper state for test

--- test/cache_tagging.c
+++ test/cache_tagging.c
@@ -377,7 +377,7 @@ get_object_header_tag(hid_t loc_id, haddr_t *tag)
     H5O_info1_t oinfo;           /* Object info */
 
     /* Retrieve the info for the object */
-    if(H5Oget_info2(loc_id, &oinfo, H5O_INFO_ALL) < 0)
+    if(H5Oget_info2(loc_id, &oinfo, H5O_INFO_BASIC) < 0)
         TEST_ERROR;
 
     /* Set the tag to return */
@@ -3001,6 +3001,7 @@ check_object_info_tags(void)
     haddr_t root_tag = HADDR_UNDEF;
     haddr_t g_tag;
     H5O_info2_t oinfo;                      /* Object info struct */
+    H5O_native_info_t ninfo;                /* Native object info struct */
 
     /* Testing Macro */
     TESTING("tag application during object info retrieval");
@@ -3041,6 +3042,7 @@ check_object_info_tags(void)
     /* ===================================== */
 
     if ( H5Oget_info_by_name3(fid, GROUPNAME, &oinfo, H5O_INFO_ALL, H5P_DEFAULT) < 0 ) TEST_ERROR;
+    if ( H5Oget_native_info_by_name(fid, GROUPNAME, &ninfo, H5O_INFO_ALL, H5P_DEFAULT) < 0 ) TEST_ERROR;
 
     /* =================================== */
     /* Verification of Metadata Tag Values */
@@ -3641,6 +3643,7 @@ check_external_link_open_tags(void)
     hid_t gid = -1;                         /* Dataspace Identifier */
     hid_t xid = -1;                         /* Dataspace Identifier */
     int verbose = FALSE;                    /* verbose file outout */
+    H5O_native_info_t ninfo;                /* Native object info struct */
     hid_t fapl = -1;         /* File access prop list */
     haddr_t root_tag = 0;
     haddr_t root2_tag = 0;
@@ -3693,6 +3696,7 @@ check_external_link_open_tags(void)
     if ( (xid = H5Gopen2(fid, LINKNAME, H5P_DEFAULT)) < 0 ) TEST_ERROR;
     if ( (fid2 = H5Iget_file_id(xid)) < 0) TEST_ERROR;
     if ( get_object_header_tag(xid, &link_tag) < 0 ) TEST_ERROR;
+    if ( H5Oget_native_info(xid, &ninfo, H5O_INFO_ALL) < 0 ) TEST_ERROR;
 
     /* =================================== */
     /* Verification of Metadata Tag Values */
